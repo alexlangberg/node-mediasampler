5b25e3bdb8547058795980fa6659312497d584fb
Alex Langberg <mail@alexlangberg.dk>
2015-10-06 23:25:36 +0200

    Increase retries.

--- debug.js
+++ debug.js
@@ -3,7 +3,7 @@
 var sitesampler = require('sitesampler');
 
 var options = {
-  'rule': { 'second': [15, 30, 45, 0] },
+  'rule': { 'second': [15, 30, 45, 1] },
   'targets': [
     {
       'url': 'http://www.google.dk',
@@ -15,6 +15,9 @@ var options = {
   'options': {
     'goldwasher': {
       'selector': 'h1, h2, h3, h4, h5, h6'
+    },
+    'retry': {
+      'retries': 10
     }
   },
   'chronostore': {
